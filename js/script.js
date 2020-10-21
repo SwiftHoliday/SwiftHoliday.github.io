1e571c907384482f86ea5c5251a97cb2d08691ba
Jonathyn Carter <jonathyncarter@gmail.com>
2020-10-20 21:52:22 -0400

    adds functional response to API

--- js/script.js
+++ js/script.js
@@ -1,5 +1,39 @@
-$('.toptext').hide();
-$('.toptext').fadeIn(7000);
 
-$('.bottomtext').hide();
-$('.bottomtext').fadeIn(11000);
+
+//$('.toptext').hide();
+//$('.toptext').fadeIn(7000);
+
+//$('.bottomtext').hide();
+//$('.bottomtext').fadeIn(10000);
+
+// Constants
+
+
+// Variables
+
+
+// Cached Element References
+
+
+// Functions 
+
+$(document).ready(function() {              // Load function on site load
+        function getNewQuote() {
+        $.ajax({
+            url:'https://api.forismatic.com/api/1.0/',
+            jsonp: 'jsonp', 
+            dataType: 'jsonp',              // Allowing access to API
+            data: {
+                method: 'getQuote',
+                lang: 'en',
+                format: 'jsonp', 
+            },
+            success: function(display) {
+                console.log(display.quoteText);
+            }
+        });
+
+        }
+        
+        getNewQuote();
+})
